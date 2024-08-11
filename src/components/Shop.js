acb1d8c821d3864db7532c39d65c2f2974ad154f
salimsheikh <salimsheikh4u2000@gmail.com>
2024-08-12 00:12:05 +0530

    Without Bind Action

---
+++ src/components/Shop.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+// import { bindActionCreators } from 'redux';
+import {actionCreators} from "../state/index"
+
+const Shop = () => {
+
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Deposit / Withdraw Money</h2>
+      <div>
+        <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}>-</button>
+        Update Balance
+        <button className='btn btn-primary mx-2' onClick={() => {dispatch(actionCreators.depositMoney(100))}}>+</button>
+      </div>
+    </div>
+  )
+}
+
+export default Shop
